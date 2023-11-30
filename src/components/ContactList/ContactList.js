6dda75480fea9bf2f379c02cf94025c1f2b30877
raspiotr <raspiotr@gmail.com>
2023-11-30 21:49:52 +0100

    with createSelector

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,21 +1,16 @@
 import { useSelector } from 'react-redux';
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const myFilter = useSelector(selectFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
-      {contacts
-        .filter(contact =>
-          contact.name.toLowerCase().includes(myFilter.toLowerCase())
-        )
-        .map(contact => (
-          <ContactListItem contact={contact} key={contact.id} />
-        ))}
+      {filteredContacts.map(contact => (
+        <ContactListItem contact={contact} key={contact.id} />
+      ))}
     </ul>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -5,3 +7,12 @@ export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, myFilter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(myFilter.toLowerCase())
+    );
+  }
+);
